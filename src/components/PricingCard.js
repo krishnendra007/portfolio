70bea01163245a17d5ba503a8ced43539c10834c
krishnendra007 <88177574+krishnendra007@users.noreply.github.com>
2024-03-23 01:11:59 +0530

    Update PricingCard.js

--- src/components/PricingCard.js
+++ src/components/PricingCard.js
@@ -6,34 +6,41 @@ const PricingCard = () => {
   return (
     <div className="pricing">
       <h1 className="exprience-heading">Professional Expriences</h1>
-        <div className="card-container">
-            <div className="card">
-            <h3> Incedo Technology Solutions</h3>
-            <span className="bar">
+      <div className="card-container">
+        <div className="card">
+          <h3> Incedo Technology Solutions</h3>
+          <span className="bar">
 
-            </span>
-            <p className="btc">Software Engineer Trainee</p>
-            <p>July 2022-Present</p>
-            <p>  <h3>Tech stacks</h3> Computer Networking, Load balancing, Linux, Harmony Controller, Advance Core Operating System,
-                  Application Firewall, Proxy Server
-            </p>
-            <p>Working with TAC team for A10 Networks to provide advance security and traffic management services and products like load balancers ,
-                application firewall to clients.
-            </p>
+          </span>
+          <p className="btc">Software Engineer </p>
+          <p>July 2022-Present</p>
+
+          <p>  <h3>Tech stacks</h3> Advanced core operating system, load balancing, Linux, Virtual Machine, Java, HTML, CSS, JavaScript.
+          </p>
+          <p>Collaborated closely with the engineering, reliability, and TAC teams at A10 Networks to deliver sophisticated networking and cybersecurity
+            solutions to a diverse clientele of business-to-consumer customers.
+          </p>
+          <p>
+            Engaged with the TAC-tools team in the development of data filtering and analysis applications.
+          </p>
+          <p>Assisted clients in the deployment, maintenance, and optimization of load balancing and traffic management solutions within their networks.</p>
         </div>
 
         <div className="card">
-            <h3> Incedo Technology Solutions</h3>
-            <span className="bar">
-            </span>
-            <p className="btc">Software Engineer intern</p>
-            <p>Jan 2022-July 2022</p>
-            <p><h3>Tech Stacks </h3>Python, MySQL, Big data, Hadoop, Hive, Cloud computing, AWS</p>
-            <p>work with D&AI team to build data pipelines and explore data using cloud capabilities.
-            </p>
-            
+          <h3> Incedo Technology Solutions</h3>
+          <span className="bar">
+          </span>
+          <p className="btc">Software Engineer intern</p>
+          <p>Jan 2022-July 2022</p>
+          <p><h3>Tech Stacks </h3>Python, MySQL, Big data, Hadoop, Hive, Cloud computing, AWS</p>
+          <p>Collaborated with the Data Engineering team on the Capstone Project to construct data pipelines and conduct data exploration leveraging cloud
+            capabilities.
+          </p>
+          <p>Received the North Star Excellence Award for the top-performing team in the Capstone project, hosted by Incedo.
+          </p>
+
         </div>
-        </div>
+      </div>
     </div>
   )
 }
