93b7f10cb52f484f8a55f3d76190e435cf55f66b
krishnendra007 <88177574+krishnendra007@users.noreply.github.com>
2024-03-23 01:36:04 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,4 @@
+// Form.js
 import "./FormStyles.css";
 import React from 'react'
 import { useNavigate } from "react-router-dom";
@@ -13,26 +14,26 @@ const Form = () => {
 
   return (
     <div >
-    <h1 className="form-heading">Contact Me</h1>
+      <h1 className="form-heading">Contact Me</h1>
       <p className="form-p">If you have any questions, feel free to drop me a line.<br/> If you don't get any answer immediately, I might just <br/> be travelling
-      throught middle of nowhere.<br/>I'll get back to you as soon as possible as I can.<br/> That's a promise!</p>
+      through the middle of nowhere.<br/>I'll get back to you as soon as possible as I can.<br/> That's a promise!</p>
     
-    <div className="form">
-      
-        <form>
-            <label>Your Name</label>
-            <input type="text"></input>
-            <label>Email</label>
-            <input type="email"></input>
-            <label>Subject</label>
-            <input type="text"></input>
-            <label>Message</label>
-            <textarea rows='6' placeholder="Type your message here"></textarea>
-          <button className="btn" onClick={routeChange}>Submit</button>
+      <div className="form">
+        <form action="https://formspree.io/f/xjvnezob" method="POST">
+          <label>Your Name</label>
+          <input type="text" name="name"></input>
+          <label>Email</label>
+          <input type="email" name="_replyto"></input>
+          <label>Subject</label>
+          <input type="text" name="subject"></input>
+          <label>Message</label>
+          <textarea rows='6' name="message" placeholder="Type your message here"></textarea>
+          <button className="btn" type="submit" onSubmit={routeChange}>Submit</button>
         </form>
-    </div>
+      </div>
     </div>
   )
 }
 
 export default Form
+
