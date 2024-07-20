14e5360331ce61bed64a9ff0e9b5025d1ade6c7c
krishnendra007 <88177574+krishnendra007@users.noreply.github.com>
2024-07-20 19:23:12 +0530

    Update AboutContent.js

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -12,13 +12,12 @@ const AboutContent = () => {
       <div className="left">
         <h1> Who Am I ?</h1>
         <p>HOLA AMIGOS !
-          My Name is Krishnendra Sharma. I had completed my graduation form Maulana Azad National Institute of Technology Bhopal in 2022. Am working as a Software Engineer Trainee in Incedo Technology Solutions Limited. Am exprienced in various domains  like front-end development, back-end development,full stack
-          , databases, data engineering, big data, computer networks, load balancing etc. Am also certified Cloud Practitioner by Amazon Web Services.
-          I have hands on working exprience with tech stacks like Data structures and algorithms, OOps, CN, C++, Java, Javascript,  Mysql, MongoDB, AWS, Linux etc.
+         My name is Krishnendra Sharma. I graduated from Maulana Azad National Institute of Technology Bhopal in 2022 and currently work as a Software Engineer at Incedo Technology Solutions Limited. I have experience in various domains, including front-end development, back-end development, full-stack development, databases, data engineering, big data, computer networks, and load balancing. I am a certified Cloud Practitioner by Amazon Web Services and an ACOS 4 Architect by A10 Networks. My hands-on experience encompasses a wide range of technologies such as data structures and algorithms, object-oriented programming (OOP), Java, Spring Boot, C++, JavaScript, MySQL, MongoDB, AWS, Linux, and load balancing. 
           <br/>
           <br/>
           Wants to know more ??
-
+           Explore my portfolio and let's connect to create something amazing!
+           <br/>
         </p>
         <Link to="/contact">
           <button  className="btn" > Contact Me</button>
